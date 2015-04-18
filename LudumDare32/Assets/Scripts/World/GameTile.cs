0fd0641763f9a1048d799c6c07ade3b37987f74c
surrealarboreal <surrealarboreal@gmail.com>
2015-04-18 13:36:35 -0300

    First clickable tiles.

---
+++ LudumDare32/Assets/Scripts/World/GameTile.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTile : MonoBehaviour 
+{
+	private MeshCollider _col;
+	private Renderer _renderer;
+	
+	void Awake()
+	{
+		_col = this.gameObject.GetComponent<MeshCollider>();
+		_renderer = this.gameObject.GetComponent<Renderer>();
+	}
+
+	// Mouse Events
+	void OnMouseEnter()
+	{
+		if(_renderer != null)
+		{
+			if(_renderer.material != null)
+			{
+				_renderer.material.color = Color.red;
+			}
+		}
+	}
+
+	void OnMouseOver()
+	{
+
+	}
+
+	void OnMouseExit()
+	{
+		if(_renderer != null)
+		{
+			if(_renderer.material != null)
+			{
+				_renderer.material.color = Color.white;
+			}
+		}
+	}
+
+	void Update() 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray mousePosRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(mousePosRay, out hit))
+			{
+				if(hit.collider == _col)
+				{
+					Debug.Log ("Click is on an object: " + this.gameObject.name);
+				}
+			}
+		}
+		if(Input.GetMouseButtonDown(1))
+		{
+			// Do something else.
+		}
+		if(Input.GetMouseButtonDown(2))
+		{
+			// yeah.
+		}
+	}
+}
