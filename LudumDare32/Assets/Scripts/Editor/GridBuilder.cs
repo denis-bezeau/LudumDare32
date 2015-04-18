1d2c83104b4e8f189827fafb86b8b1ad5d2d5bf7
surrealarboreal <surrealarboreal@gmail.com>
2015-04-18 16:59:10 -0300

    Final Room Builder with tags and room collider.

--- LudumDare32/Assets/Scripts/Editor/GridBuilder.cs
+++ LudumDare32/Assets/Scripts/Editor/GridBuilder.cs
@@ -5,13 +5,13 @@ using System.Collections;
 
 public class GridBuilder : EditorWindow
 {
-	private int _gridWidth = 5;
-	private int _gridHeight = 5;
+	private int _roomWidthTiles = 5;
+	private int _roomHeightTiles = 5;
 	private string _roomName = "room";
 	private GameEnums.RoomType _roomType = GameEnums.RoomType.Normal;
 
-	private static readonly int kRoomWidth = 10;
-	private static readonly int kRoomHeight = 10;
+	private static readonly int kTileWidth = 10;
+	private static readonly int kTileHeight = 10;
 	private static readonly string kRoomPath = "Assets/Resources/Prefabs/Rooms/";
 	private static readonly string kRoomSuffix = ".prefab";
 
@@ -29,8 +29,8 @@ public class GridBuilder : EditorWindow
 	{
 		GUILayout.Label ("Build a Grid of size.", EditorStyles.boldLabel);
 
-		_gridWidth = EditorGUILayout.IntField("Room Width: ", _gridWidth);
-		_gridHeight = EditorGUILayout.IntField("Room Height ", _gridHeight);
+		_roomWidthTiles = EditorGUILayout.IntField("Room Width: ", _roomWidthTiles);
+		_roomHeightTiles = EditorGUILayout.IntField("Room Height ", _roomHeightTiles);
 		_roomName = EditorGUILayout.TextField("Room Name: ", _roomName);
 
 		_roomType = (GameEnums.RoomType)EditorGUILayout.EnumPopup ("Room Type", _roomType);
@@ -60,6 +60,18 @@ public class GridBuilder : EditorWindow
 				roomObj.gameObject.AddComponent<SpawnRoom>();
 			}
 
+
+			roomObj.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+
+			BoxCollider col = roomObj.gameObject.AddComponent<BoxCollider>();
+
+			col.size = new Vector3(kTileWidth * _roomWidthTiles, 5.0f, kTileHeight * _roomHeightTiles);
+
+			float colCenterX = ((kTileWidth * _roomWidthTiles) / 2.0f) - (kTileWidth / 2.0f);
+			float colCenterY = ((kTileHeight * _roomHeightTiles) / 2.0f) - (kTileHeight / 2.0f);
+		
+			col.center = new Vector3(colCenterX , 0.0f, colCenterY);
+
 			for(int i = 0; i < roomObj.transform.childCount; i++)
 			{
 				GameObject.DestroyImmediate(roomObj.transform.GetChild(i));
@@ -67,14 +79,15 @@ public class GridBuilder : EditorWindow
 
 			int tileCount = 1;
 
-			for(int i = 0; i < _gridWidth; i++)
+			for(int i = 0; i < _roomWidthTiles; i++)
 			{
-				for(int j = 0; j < _gridHeight; j++)
+				for(int j = 0; j < _roomHeightTiles; j++)
 				{
 					GameObject tile = GameObject.Instantiate(_tilePrefab);
-					tile.transform.localPosition = new Vector3(kRoomWidth * i , 0.0f, kRoomHeight * j);
+					tile.transform.localPosition = new Vector3(kTileWidth * i , 0.0f, kTileHeight * j);
 					tile.name = "Tile" + tileCount;
 					tile.transform.parent = roomObj.transform;
+					tile.layer = LayerMask.NameToLayer("Tiles");
 					tileCount++;
 				}
 			}
@@ -87,7 +100,6 @@ public class GridBuilder : EditorWindow
 			GameObject.DestroyImmediate(roomObj);
 
 			PrefabUtility.InstantiatePrefab(prefab);
-
 		}
 	}
 }
