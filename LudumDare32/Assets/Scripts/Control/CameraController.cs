3ff1ebc54db1086e406de60c7f6b4d93c71915c8
surrealarboreal <surrealarboreal@gmail.com>
2015-04-19 13:00:14 -0300

    Vector Extensions and a cheap camera controller.

--- LudumDare32/Assets/Scripts/Control/CameraController.cs
+++ LudumDare32/Assets/Scripts/Control/CameraController.cs
@@ -1,37 +1,46 @@
-﻿using UnityEngine;
+﻿//In a file MaxCamera.cs
+using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour 
+public class CameraController : MonoBehaviour
 {
-	[SerializeField]
-	private float _mouseSensitivity = 0.01f;
-	private Vector3 _lastPosition;
+	public float mouseSensitivity = 1.0f;
+	public float maxDeltaX = 10.0f;
+	public float maxDeltaY = 5.0f;
 
-	// Constraints
-	[SerializeField]
-	private LevelAnchor _levelBounds; 
+	private Vector3 lastPosition;
+	private Vector3 cameraStartPos;
+	private Vector3 cameraWorldPos;
 	
-	void Update()
+	void Start()
 	{
-		Vector3 screenPos = _levelBounds.gameObject.transform.InverseTransformPoint(Input.mousePosition);
-		screenPos = Camera.main.ScreenToWorldPoint(screenPos);
+		cameraStartPos = this.transform.position;
+	}
+	
+	void FixedUpdate()
+	{
+		cameraWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 		if (Input.GetMouseButtonDown(1))
 		{
-			if(_levelBounds.GetBounds.Contains(screenPos))
-			{
-				_lastPosition = screenPos;
-			}
+			lastPosition = cameraWorldPos;
 		}
 		
 		if (Input.GetMouseButton(1))
 		{
-			if(_levelBounds.GetBounds.Contains(screenPos))
+			Vector3 delta = cameraWorldPos - lastPosition;
+			float dX = delta.x * mouseSensitivity;
+			float dY = delta.y * mouseSensitivity;
+
+			float totalDeltaX = dX + transform.position.x;
+			float totalDeltaY = dY + transform.position.y;
+
+			if (Mathf.Abs (totalDeltaX - cameraStartPos.x) < maxDeltaX && Mathf.Abs(totalDeltaY - cameraStartPos.y) < maxDeltaY)
 			{
-				Vector3 delta = screenPos - _lastPosition;
-				this.transform.Translate(delta.x * _mouseSensitivity, delta.y * _mouseSensitivity, 0.0f);
-				_lastPosition = screenPos;
+				transform.Translate(dX, dY, 0);
+				lastPosition = cameraWorldPos;
 			}
 		}
 	}
 }
+
---
+++ LudumDare32/Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Global Game Jam 2015 Vector Extensions
+public static class VectorExtensions 
+{
+	/// Vector 2 Extensions
+	public static Vector2 ToVector2YZ (this Vector3 v) 
+	{
+		return new Vector2 (v.y, v.z);
+	}
+
+	public static Vector2 ToVector2XZ (this Vector3 v) 
+	{
+		return new Vector2 (v.x, v.z);
+	}
+
+	public static Vector2 ToVector2XY (this Vector3 v) 
+	{
+		return new Vector2 (v.x, v.y);
+	}
+
+	/// Vector 3 Extensions
+	public static Vector3 ToVector3XZ (this Vector2 v) 
+	{
+		return new Vector3 (v.x, 0.0f, v.y);
+	}
+
+}
